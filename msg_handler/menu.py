6c9d74128d39de5731c5696833b58fb4c80734ba
Petrus J.v.Rensburg <petrus.jvrensburg@gmail.com>
2014-02-20 12:16:37 +0200

    Refactor USSD menu, to make the content fit into the 140 char limit.

--- msg_handler/menu.py
+++ msg_handler/menu.py
@@ -1,61 +1,14 @@
 menu = {
-    "title": "Main menu",
+    "title": "Housing Development Questions",
     "content": [
         {
-            "title": "I have a question about the Dido Valley Housing Development.",
-            "content": [
-                {
-                    "title": "Will I benefit?",
-                    "content": [
-                        "The final allocations are still being developed. Members of the Red Hill Informal Settlement \
-                        are being prioritised. Around 550 stands will be developed.",
-                        ]
-                },
-                {
-                    "title": "When will the project be finalised?",
-                    "content": [
-                        "Most houses are projected to be built by the end of 2016.",
-                        ]
-                },
-                {
-                    "title": "Where is the project being developed?",
-                    "content": [
-                        "The current site is on Dido Valley Road and Scala Road (where the old navy houses \
-                        burnt down).",
-                        ]
-                },
-                {
-                    "title": "What is the latest update?",
-                    "content": [
-                        "The Freshwater Consultant in consultation with the Civil Engineer determined that the water \
-                        course area plus a 20m wide zone adjacent thereto be kept free of development. The \
-                        recommendations have been sent to the Department of Water Affairs.",
-                        ]
-                },
-                {
-                    "title": "When is the next Dido Valley Steering Committee Meeting?",
-                    "content": [
-                        "A next meeting has not yet been scheduled. However, it is likely the next meeting will be \
-                        held when the Department of Water affairs have given feedback on the latest proposal.",
-                        ]
-                },
-                {
-                    "title": "Where can I get more information?",
-                    "content":[
-                        "There is a list of frequently asked questions and further contact details on the website \
-                        linked to this system at [xx].",
-                        ]
-                },
-                ]
-        },
-        {
-            "title": "I have a question about housing issues in general.",
+            "title": "General issues",
             "content": [
                 {
                     "title": "Where do I find out about the housing database?",
                     "content": [
-                        "Housing database queries can be emailed to housingdb@capetown.gov.za. \
-                        Alternatively contact City Housing for Ocean View at 021 783 1861.",
+                        "Housing database queries can be emailed to housingdb@capetown.gov.za.",
+                        "Alternatively contact City Housing for Ocean View at 021 783 1861.",
                         ]
                 },
                 {
@@ -67,21 +20,61 @@ menu = {
                 ]
         },
         {
-            "title": "I have a question about the Red Hill Informal Settlement.",
+            "title": "Dido Valley",
+            "content": [
+
+                {
+                    "title": "Will I benefit?",
+                    "content": [
+                        "The final allocations are still being developed.",
+                        "Members of the Red Hill Informal Settlement are being prioritised.",
+                        "Around 550 stands will be developed.",
+                        ]
+                },
+                {
+                    "title": "When will the project be finalised?",
+                    "content": [
+                        "Most houses are projected to be built by the end of 2016.",
+                        ]
+                },
+                {
+                    "title": "Where is the development?",
+                    "content": [
+                        "The current site is on Dido Valley Road and Scala Road (where the old navy houses burnt down).",
+                        ]
+                },
+                {
+                    "title": "Latest update?",
+                    "content": [
+                        "The Freshwater Consultant and the Civil Engineer determined that the water course area...",
+                        "...plus a 20m wide zone adjacent thereto be kept free of development.",
+                        "The recommendations have been sent to the Department of Water Affairs.",
+                        ]
+                },
+                {
+                    "title": "More info?",
+                    "content":[
+                        "The next steering committee meeting has not yet been scheduled.",
+                        "The next meeting will likely be held only after getting feedback from the Department of Water Affairs.",
+                        "Further contact details and info can be found on the internet at [xx].",
+                        ]
+                },
+                ]
+        },
+        {
+            "title": "Red Hill Informal Settlement",
             "content": [
                 {
                     "title": "Give me my Ward Councillor contacts.",
                     "content": [
-                        "Your Councilor is Simon Liell-Cock. His number is 082 551 7203 and email is \
-                        simon.liell-cock@capetown.gov.za. The Sub Councilor is Patricia Francke. Her \
-                        email is Patricia.Francke@capetown.gov.za.",
+                        "Your Councilor is Simon Liell-Cock (082 551 7203, email simon.liell-cock@capetown.gov.za).",
+                        "The Sub Councilor is Patricia Francke (email Patricia.Francke@capetown.gov.za).",
                         ]
                 },
                 {
                     "title": "Give me the details of the Red Hill Community Committee",
                     "content": [
-                        "You can contact the Steering Committee through the chair Luyando Lombo at email \
-                        luyanda.lombo@sanparks.org.",
+                        "You can contact the committee chair: Luyando Lombo (email luyanda.lombo@sanparks.org).",
                         ]
                 },
                 ]
